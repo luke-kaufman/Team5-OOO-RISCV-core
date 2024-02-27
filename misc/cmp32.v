da6ca1b128b2e39fca731a5aa30cd20e9fedc670
Orhan Ünüvar <ounuva@gmail.com>
2024-02-27 17:36:55 +0300

    fix cmp32 bug

--- misc/cmp32.v
+++ misc/cmp32.v
@@ -5,13 +5,13 @@ module cmp32 (
     input wire [31:0] b,
     output wire y
 );
-    wire [31:0] z;
+    wire [31:0] eq;
 
     generate
         for (genvar i = 0; i < 32; i = i + 1) begin
-            XOR2_X1 xor_gate(.A(a[i]), .B(b[i]), .Z(z[i]));
+            XNOR2_X1 xnor_(.A(a[i]), .B(b[i]), .ZN(eq[i]));
         end
     endgenerate
 
-    and32 and_gate(.a(z), .y(y));
+    and32 and_(.a(eq), .y(y));
 endmodule
